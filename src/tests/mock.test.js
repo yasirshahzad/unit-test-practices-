03689cb26c3d5b5d88b7c780fe5e7d6aa4bae5f0
Muhammad Yasir <shahzadyasir77@gmail.com>
2024-11-09 10:20:49 +0500

    created test mocking

--- src/tests/mock.test.js
+++ src/tests/mock.test.js
@@ -1,12 +1,20 @@
 import { vi, it, expect, describe } from "vitest";
 
 describe("testing mocking", () => {
-  it("should return hi", async () => {
-    const greetings = vi.fn();
+  it("should return ok", () => {
+    // Create a mock for the following function
+    const sendText = vi.fn();
 
-    greetings.mockResolvedValue("hi");
-    const results = await greetings();
+    // sendText(message) { }
+    sendText.mockReturnValue("ok");
 
-    expect(results).toBe("hi");
+    // Call the mock function
+    const result = sendText("message");
+
+    // Assert that the mock function is called
+    expect(sendText).toHaveBeenCalledWith("message");
+
+    // Assert that the result is ok
+    expect(result).toBe("ok");
   });
 });
