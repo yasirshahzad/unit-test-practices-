fc81a2c9e2a3650c76cf4fce4813034f6506d1c3
Muhammad Yasir <shahzadyasir77@gmail.com>
2024-11-10 13:02:27 +0500

    getShippingInfo

--- src/tests/mock.test.js
+++ src/tests/mock.test.js
@@ -1,9 +1,11 @@
 import { vi, it, expect, describe } from "vitest";
-import { getPriceInCurrency } from "../mocking";
+import { getPriceInCurrency, getShippingInfo } from "../mocking";
 import { getExchangeRate } from "../libs/currency";
+import { getShippingQuote } from "../libs/shipping";
 
 // mocking whole module
 vi.mock("../libs/currency.js");
+vi.mock("../libs/shipping.js");
 
 describe("testing mocking", () => {
   it("should return ok", () => {
@@ -32,5 +34,26 @@ describe("getPriceInCurrency", () => {
     const result = getPriceInCurrency(100, "USD");
 
     expect(result).toBe(200);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable when quote is null", () => {
+    // Create a mock for the getShippingQuote function
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+
+    const result = getShippingInfo("New York");
+
+    expect(result).toMatch(/Shipping Unavailable/i);
+  });
+
+  it("should return shipping when quote is available", () => {
+    // Create a mock for the getShippingQuote function
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+
+    const result = getShippingInfo("New York");
+
+    // output: Shipping Cost: $${quote.cost} (${quote.estimatedDays} Days)
+    expect(result).toMatch(/Shipping Cost: \$10 \(2 Days\)/i);
   });
 });
