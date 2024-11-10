7f0f0cc7b26ea0edfbffb36ef5799ef791e2740b
Muhammad Yasir <shahzadyasir77@gmail.com>
2024-11-10 12:45:50 +0500

    getPriceInCurrency

--- src/tests/mock.test.js
+++ src/tests/mock.test.js
@@ -1,4 +1,8 @@
 import { vi, it, expect, describe } from "vitest";
+import { getPriceInCurrency } from "../mocking";
+import { getExchangeRate } from "../libs/currency";
+
+vi.mock("../libs/currency.js");
 
 describe("testing mocking", () => {
   it("should return ok", () => {
@@ -16,5 +20,16 @@ describe("testing mocking", () => {
 
     // Assert that the result is ok
     expect(result).toBe("ok");
+  });
+});
+
+describe("getPriceInCurrency", () => {
+  it("should return the currency rate", () => {
+    // Create a mock for the getExchangeRate function
+    vi.mocked(getExchangeRate).mockReturnValue(2);
+
+    const result = getPriceInCurrency(100, "USD");
+
+    expect(result).toBe(200);
   });
 });
